048708253e03a5bb8162058781b080adf0acbe1f
Romeu I. L. Pires <rilpires@gmail.com>
2019-05-04 21:48:42 -0300

    pylons resolved

---
+++ golf_gophers.cpp
@@ -0,0 +1,28 @@
+/*
+    Made by: Romeu I. L. Pires
+    for "Special topics in programming" course
+    in UFRJ (Universidade Federal do Rio de Janeiro),
+    on 2019.1 semester
+
+    - Problem URL (Google Code Jam 2019):
+        https://codingcompetitions.withgoogle.com/codejam/round/0000000000051635/0000000000104f1a
+
+*/
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    ifstream cin("entrada.txt");
+    ofstream cout("saida.txt");
+    #endif
+    // ==========
+}
